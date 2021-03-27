15c14156cb86f743085618627fa5ceddc2eeef5b
fujisawa reon <fujisawa@reonotis.jp>
2021-03-27 18:43:16 +0900

    test

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -246,6 +246,8 @@ class ApprovalController extends Controller
      *
      */
     public function register_WP_courseSchedules($id,$course_schedules){
+
+        dd("111111111111");
         $lastID = DB::connection('mysql_2')->table('my_schedule')->insertGetId([
             'date' => $course_schedules->date,
             'open_time' => $course_schedules->time,
