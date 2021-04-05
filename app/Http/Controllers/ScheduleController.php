306ee9e5cbbaf55fb6eff8f003d2233edaa20960
fujisawa reon <fujisawa@reonotis.jp>
2021-04-05 22:15:28 +0900

    test

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -114,7 +114,7 @@ class ScheduleController extends Controller
         $month = substr($DATE, 0, 7);
         if(isset($_GET['month']))$month = $_GET['month'];
 
-        dd($DATE);
+        dd($month);
         $NISSUU = (date('t', strtotime($DATE)));
         $query = CustomerSchedule::select(DB::raw('count(*) as customer_count'), 'customer_schedules.*', 'customers.name as customerName', 'courses.course_name' )
             ->leftJoin('customers', 'customers.id', '=', 'customer_schedules.customer_id')
