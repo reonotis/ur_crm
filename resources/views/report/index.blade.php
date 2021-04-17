c7f1a08ec081446a6eaa36e4cb120f07b94a8eee
fujisawa reon <fujisawa@reonotis.jp>
2021-04-17 20:29:46 +0900

    不要なモデルを削除

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">日報</div>
+                <div class="card-header">売上確認</div>
 
                 <div class="card-body">
                     
