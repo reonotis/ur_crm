37ac4c6b601f2cb474ea3febf52f9aeea9e47cc7
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2023-03-25 21:58:44 +0900

    サンプルメール送信用batch作成

---
+++ app/Console/Commands/MailTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\Base;
+use Exception;
+use Illuminate\Support\Facades\Mail;
+
+/**
+ *
+ */
+class MailTest extends Base
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:MailTest';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '管理者のアドレスにメールを送信します';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function handle(): void
+    {
+        $this->writeConsoleAndLog('info', $this->name . ' start...');
+
+        $data = [
+            "name" => "aaaaaa",
+        ];
+        Mail::send('emails.sample', $data, function ($message) {
+            $message->to('fujisawareon@yahoo.co.jp')
+                ->from('info@newbalance-campaign.jp')
+                ->bcc("fujisawareon@yahoo.co.jp")
+                ->subject('お申込みありがとうございます。');
+        });
+
+        $this->writeConsoleAndLog('info', $this->name . ' finish.');
+    }
+
+}
